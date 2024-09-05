750b702bb306676c9f33d9168e677e398cda52a3
Eleanor Prins <eleanor@prins.life>
2024-09-06 11:46:06 +1200

    Create push_build_and_tag.sh

---
+++ push_build_and_tag.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# App name
+app_name="colourchooser"
+username="joyte"
+
+# Push to github
+git push
+
+# Get the latest tag
+latest_tag=$(git log -n1 --format="%h")
+
+# Docker build with :latest and :$latest_tag
+docker build -t $username/$app_name:latest -t $username/$app_name:$latest_tag .
+
+# Docker push both tags
+docker push $username/$app_name:latest
+docker push $username/$app_name:$latest_tag
+
+# Done
